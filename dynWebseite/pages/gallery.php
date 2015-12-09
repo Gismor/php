7c5ebb00aefe29ee40719eb9b9e10c55eae5e8ab
dneubauer <davidneubauer85@gmail.com>
2015-12-09 11:36:18 +0100

    update

--- dynWebseite/pages/gallery.php
+++ dynWebseite/pages/gallery.php
@@ -1,13 +1,36 @@
 <?php
 
-$gallery = createGallery('./bilder/',7,100);
+$gallery = createGallery('./bilder/', 7, 100);
 echo $gallery;
 function createGallery($srcdir, $n, $width, $height = null)
 {
 
-    $bilder = glob($srcdir.'*.jpg');
+    $bilder = glob($srcdir . '*.jpg');  // Array mit Bildern
+
+    $string = "<table>";
+    $string .= "<tbody>";
+    $string .= "<tr>";
+
+    $counter = 1;
 
 
+    foreach ($bilder as $bild) {
+        $string .= "<td>";
+        $string .= "<img src='$bild' width='$width'>";
+        $string .= "</td>";
+        if ($counter++ == $n) {
+            $counter = 1;
+            $string .= '</tr><tr>';
+        }
+
+    }
+
+    $string .= "</tr>";
+    $string .= "</tbody>";
+    $string .= "</table>";
+
+
+    return $string;
 
 }
 
